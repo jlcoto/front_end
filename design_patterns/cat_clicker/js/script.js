12324e5985024f6ab9bf8884067ca7fecb2ad239
jlcoto <jscoto@gmail.com>
2017-03-07 14:17:32 +0100

    Make code more VOM.

--- design_patterns/cat_clicker/js/script.js
+++ design_patterns/cat_clicker/js/script.js
@@ -1,73 +1,108 @@
-
-var Cats = function(pic, num) {
-	this.pic = pic;
-	this.id = 'cat_' + num;
-	this.clicking = 0;
-};
-
-Cats.prototype.showPic = function() {
-	$("#cat_image").attr('src', 'img/' + this.pic);
-	$("#number").text(this.clicking);
-
-}
-
-Cats.prototype.clickCounter = function() {
-	var clicker = this.clicking;
-	$('#cat_image').click(function(e) {
-		clicker += 1;
-		$('#number' ).text(clicker);
-	})
-	this.clicking = clicker;
-};
-
-
-
-var catPics = {
+$(function(){
+var data = {
 	"cats": [{"name": "kitty 1",
-	"image": 'cat_pic.jpg',
+				"image": 'cat_pic.jpg',
+				"clicks": 0
 		},
 		{"name": "kitty 2",
-		"image": 'cat_pic_2.jpg'},
+		"image": 'cat_pic_2.jpg',
+		"clicks": 0},
 		{"name": "kitty 3",
-		"image": 'cat_pic_3.jpg'
+		"image": 'cat_pic_3.jpg',
+		"clicks":0
+		},
+		{"name": "kitty 4",
+		"image": 'cat_pic_4.jpg',
+		"clicks": 0
 		}
 	]
 }
 
-$("body").append('<ul id="cat-list"> </ul>');
 
-catPics.cats.forEach(function(cat, index){
-	$("#cat-list").append('<li id='+ index +'>'+cat.name+'</li>');
+var octopus = {
+	init: function(){
+		view.init()
+		view.renderCatList()
+		view.renderCatImage()
+		view.catClicker()
 
-	catPics.cats[index].kitten = new Cats(cat.image, index)
+	},
+	getCatName: function(){
+		var catName = [];
+		data.cats.forEach(function(cat){
+			catName.push(cat.name);
+		});
+		return catName;
+	},
+	getCatImage: function(name){
+		var catImage;
+		data.cats.forEach(function(cat){
+			if (cat.name == name){
+				catImage = cat.image;
+			}
+		});
+		return catImage;
+	},
+	getClick: function(name){
+		var clicks
+		data.cats.forEach(function(cat){
+			if(cat.name == name){
+				return clicks = cat.clicks
+			}
+		})
+		return clicks;
+	},
+	addClick: function(name){
+		data.cats.forEach(function(cat){
+			if(cat.name == name){
+				cat.clicks += 1;
+			}
+		})
+	}
 
-})
-
-$("body").append('<img id=cat_image>')
-$("body").append('<div id="click-counter"> Number of clicks: <span id=number> 0 </span></div>')
-
-$("#cat-list li").on('click', function(){
-	var catIndex = $(this).attr('id');
-	catPics.cats[catIndex].kitten.showPic()
-	catPics.cats[catIndex].kitten.clickCounter()
-})
+}
 
 
 
+var view = {
+	init: function(){
+		$("body").append('<ul id="cat-list"> </ul>');
+		$("body").append('<img class="cat-image">');
+		$("body").append('<div id="click-counter"> Number of clicks: <span id=number> 0 </span></div>');
+		$("#click-counter").hide()
+	},
+	renderCatList: function() {
+		octopus.getCatName().forEach(function(cat){
+			$("#cat-list").append('<li id='+ cat + '>'+cat+'</li>');
+		})
+	},
+	renderCatImage: function(){
+		$("#cat-list li").on('click', function(){
+			var selCat = $(this).text();
+			var imgCat = octopus.getCatImage(selCat);
+			var clicks = octopus.getClick(selCat);
+			$(".cat-image").attr({
+				id:  selCat,
+				src: 'img/' + imgCat,
+			});
+			$("#click-counter").show();
+			$("#number").text(clicks);
+		});
+	},
+	catClicker: function(){
+		$(".cat-image").on('click', function(){
+			var picClicked = $(this).attr('id');
+			var newClick = octopus.addClick(picClicked);
+			var clicks = octopus.getClick(picClicked);
+			$("#number").text(clicks);
+	});
+}
+}
+octopus.init()
+});
 
-// first = new Cats('cat_pic.jpg', 1);
-// first.showPic();
-// first.clickCounter();
 
 
-// var kittens = [];
 
-// catPics.forEach(function(pic, index) {
-// 	kittens.push(new Cats(pic, index))
 
-// })
 
-// kittens.forEach(function(cat) {
-// 	cat.showPic()
-// 	cat.clickCounter()
-// })
