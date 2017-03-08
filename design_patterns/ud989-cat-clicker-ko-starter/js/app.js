64cba76ad03b9cdd6e08b1255dc56b370eba2cd7
jlcoto <jscoto@gmail.com>
2017-03-08 16:57:28 +0100

    Add different cats and change cats when clicking its name.

--- design_patterns/ud989-cat-clicker-ko-starter/js/app.js
+++ design_patterns/ud989-cat-clicker-ko-starter/js/app.js
@@ -1,10 +1,40 @@
-var Cat = function(){	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.nicknames = ko.observableArray([{nick: "kitty"},
-										{nick: "michi"},
-										{nick: "miau"}
-					]);
+var dataCat = [
+{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		nicknames: [{nick: "kitty"}, {nick: "michi"}, {nick: "miau"}]
+},
+{
+		clickCount: 0,
+		name: 'Tommy',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		nicknames: [{nick: "tobby"}, {nick: "ricky"}]
+},
+{
+		clickCount: 0,
+		name: 'Kathy',
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		nicknames: [{nick: "kicky"}, {nick: "lolo"}]
+},
+{
+		clickCount: 0,
+		name: 'Happy',
+		imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+		nicknames: [{nick: "yippi"}, {nick: "cali"}]
+},
+{
+		clickCount: 0,
+		name: 'Sleepy',
+		imgSrc: 'img/9648464288_2516b35537_z.jpg',
+		nicknames: [{nick: "zzz"}, {nick: "sueño"}]
+}
+]
+
+var Cat = function(data){	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames);
 	this.levels = ko.computed(function(){
 		if (this.clickCount() < 10) {
 			return "New Born"
@@ -21,11 +51,22 @@ var Cat = function(){	this.clickCount = ko.observable(0);
 }
 
 var ViewModel = function() {
+	var self = this;
 
-	this.currentCat = ko.observable(new Cat());
+	this.catList = ko.observableArray([]);
+
+	dataCat.forEach( function(catItem){
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+	this.changeCat = function(clickedCat){
+		self.currentCat(clickedCat);
+	};
 
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
 
 
