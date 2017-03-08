c1c6ec342d41df17ff37deb6804adce058e000d9
jlcoto <jscoto@gmail.com>
2017-03-08 15:35:17 +0100

    Add separate cat function.

--- design_patterns/ud989-cat-clicker-ko-starter/js/app.js
+++ design_patterns/ud989-cat-clicker-ko-starter/js/app.js
@@ -1,12 +1,10 @@
-var ViewModel = function() {
-	this.clickCount = ko.observable(0);
+var Cat = function(){	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
-
+	this.nicknames = ko.observableArray([{nick: "kitty"},
+										{nick: "michi"},
+										{nick: "miau"}
+					]);
 	this.levels = ko.computed(function(){
 		if (this.clickCount() < 10) {
 			return "New Born"
@@ -20,6 +18,17 @@ var ViewModel = function() {
 			return "Old"
 		}
 	}, this);
+}
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat());
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+
+
 }
 
 ko.applyBindings(new ViewModel());
