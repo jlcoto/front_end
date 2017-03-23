05f93e2ecb699c6bb7a2753ff644a1c79f1a2162
jlcoto <jscoto@gmail.com>
2017-03-23 12:54:40 +0100

    Makes map and menu responsive.

--- projects/google_maps_api/js/responsive.js
+++ projects/google_maps_api/js/responsive.js
@@ -12,3 +12,19 @@
       main.addEventListener('click', function() {
         drawer.classList.remove('open');
       });
+
+if ($(window).width() > 549) {
+  $('aside').addClass('col-xs-4');
+  $('main').addClass('col-xs-8');
+}
+
+$(window).resize(function(){
+  var windowWidth = $(window).width();
+  if(windowWidth > 549){
+    $('aside').addClass('col-xs-4');
+    $('main').addClass('col-xs-8');
+  } else {
+    $('aside').removeClass('col-xs-4');
+    $('main').removeClass('col-xs-8');
+  }
+})
