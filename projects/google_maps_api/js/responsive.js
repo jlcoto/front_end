deaf12bd0b29ae753f74e7ccb3f313d294013032
jlcoto <jscoto@gmail.com>
2017-03-24 17:16:12 +0100

    Make sandwich menu for all sizes. Make map full page.

--- projects/google_maps_api/js/responsive.js
+++ projects/google_maps_api/js/responsive.js
@@ -1,30 +1,15 @@
-      /*
-       * Open the drawer when the menu ison is clicked.
-       */
-      var clickSandwich = document.querySelector('#sandwich-menu');
-      var main = document.querySelector('main');
-      var drawer = document.querySelector('#menu');
+/*
+ * Open the drawer when the menu ison is clicked.
+ */
+var clickSandwich = document.querySelector('#sandwich-menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#menu');
 
-      clickSandwich.addEventListener('click', function(e) {
-        drawer.classList.toggle('open');
-        e.stopPropagation();
-      });
-      main.addEventListener('click', function() {
-        drawer.classList.remove('open');
-      });
+clickSandwich.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
 
-if ($(window).width() > 549) {
-  $('aside').addClass('col-xs-4');
-  $('main').addClass('col-xs-8');
-}
-
-$(window).resize(function(){
-  var windowWidth = $(window).width();
-  if(windowWidth > 549){
-    $('aside').addClass('col-xs-4');
-    $('main').addClass('col-xs-8');
-  } else {
-    $('aside').removeClass('col-xs-4');
-    $('main').removeClass('col-xs-8');
-  }
-})
